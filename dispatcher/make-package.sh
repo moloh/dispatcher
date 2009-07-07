001e03c6b5bfc3d46680cc832055583c1298f363
rbroemeling <remi@infinitewinter.com>
2009-07-07 18:49:42 +0000

    * Call autoconf before building a source package to be sure we have updated   autoconf files.
    git-svn-id: file:///home/michal/Downloads/dispatcher.repository/trunk@49 0758a9ac-a79e-4c80-b831-92d37f0f7549

--- dispatcher/make-package.sh
+++ dispatcher/make-package.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -e
 
+autoconf
+
 RELEASE="$(grep PACKAGE_VERSION= ./configure | sed -e 's/[^0-9.]//g')"
 REVISION="$(svn info . | grep '^Revision: ' | sed -e 's/[^0-9]//g')"
 PACKAGE="nexopia-dispatcher-${RELEASE}-r${REVISION}"
+
 echo "Building source package '${PACKAGE}.tar.gz'."
 svn export . "${PACKAGE}"
 tar cvfz "${PACKAGE}.tar.gz" "${PACKAGE}"
